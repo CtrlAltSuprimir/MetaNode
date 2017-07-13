c7d06d46769c72cf8b9f781ccefa0e2c5b97f095
CtrlAltSuprimir <proyectodeinformatico@gmail.com>
2017-07-13 14:00:27 +0200

    // QUitada conexion MongoDB

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 
-mongoose.connect(config.db, (err, res) => {
+/*mongoose.connect(config.db, (err, res) => {
 	if(err) return console.log(`Tenemos un error en el sistema: ${err}`)
 
 	app.listen(config.port, () => {
 		console.log(`HOLA BUENAS TARDES!!! Estoy funcionando en el puerto ${config.port}`);
 	})
-})
+})*/
